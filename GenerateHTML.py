5c38cf866bef0fd468112c0f6ef5aa52e076c98e
Peldon <peldon@quantentunnel.de>
2022-05-25 20:57:35 +0200

    Updated from google sheet - up to 31.12.2020

--- GenerateHTML.py
+++ GenerateHTML.py
@@ -1,11 +1,11 @@
 
 class KritzelKaffeeTweet:
-    def __init__(self, id, date, imglink, text, name):
-         self.id = id
-         self.date = date
-         self.imglink = imglink
-         self.text = text
-         self.name = name
+    def __init__(self, kritzelid, date, imglink, text, name):
+        self.kritzelid = kritzelid
+        self.date = date
+        self.imglink = imglink
+        self.text = text
+        self.name = name
 
 
 def convert_to_KritzelKaffeeTweet(line):
@@ -14,7 +14,7 @@ def convert_to_KritzelKaffeeTweet(line):
 def read_csv():
     import csv
     filename = "KritzelKaffeesWithNames.csv" 
-    kritzelkaffees = []
+    kritzelkaffeesliste = []
     with open(filename, 'r', encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         rownr = 0
@@ -22,18 +22,18 @@ def read_csv():
             rownr += 1
             if (rownr == 1): #ignore header
                 continue
-            kritzelkaffees.append(convert_to_KritzelKaffeeTweet(row))
-    return kritzelkaffees
+            kritzelkaffeesliste.append(convert_to_KritzelKaffeeTweet(row))
+    return kritzelkaffeesliste
 
-def save_html(kritzelkaffees):
+def save_html(kritzelkaffeesliste):
     filename = "KritzelKaffees.html"
     with open('html/'+filename, 'w', encoding="utf-8") as htmlfile:
         from HtmlTemplate import htmlstart, htmlend
         htmlfile.write(htmlstart)
-        for k in kritzelkaffees:
+        for k in kritzelkaffeesliste:
             htmlfile.write("<div class=\"kritzelkaffee-div\">\n")
             htmlfile.write("<table class=\"kritzelkaffee-table\">\n")
-            htmlfile.write("<tr><td class=\"kritzelkaffee-table-date\"><a href='https://twitter.com/datGestruepp/status/" + k.id + "'>Tweet vom "+k.date+"</a></td></tr>\n")
+            htmlfile.write("<tr><td class=\"kritzelkaffee-table-date\"><a href='https://twitter.com/datGestruepp/status/" + k.kritzelid + "'>Tweet vom "+k.date+"</a></td></tr>\n")
             htmlfile.write("<tr><td class=\"kritzelkaffee-table-name\">"+k.name+"</td></tr>\n")
             htmlfile.write("<tr><td class=\"kritzelkaffee-table-img\"><img src=\"placeholder.svg\" data-src='"+k.imglink+"'></td></tr>\n")
             htmlfile.write("<tr><td class=\"kritzelkaffee-table-text\">"+k.text+"</td></tr>\n")
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     print('Looking for KritzelKaffee csv file')
     kritzelkaffees = read_csv()
     print('Found ' + str(len(kritzelkaffees)) + ' KritzelKaffee entries!')
-    kritzelkaffees.sort(key=lambda k: k.id, reverse=True)
+    kritzelkaffees.sort(key=lambda k: k.kritzelid, reverse=True)
     htmlfilename = save_html(kritzelkaffees)
     print('Wrote data to ' + htmlfilename)
     print('Bye')
